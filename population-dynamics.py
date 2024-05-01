ff40c8b3938018856663adca8fc3398470ca654e
Kamilly-Carvalho <carvalho.kamilly@academico.ifpb.edu.br>
2024-05-01 16:58:32 -0300

    adaptation of the code for population dynamics in a dengue infection

--- population-dynamics.py
+++ population-dynamics.py
@@ -34,10 +34,10 @@ v = np.zeros((pregnancy,time,kutta))
 i = np.zeros((pregnancy,time,kutta))
 z = np.zeros((pregnancy,time,kutta))
 
-s_1 = np.zeros((pregnancy+9,time,kutta))
-v_1 = np.zeros((pregnancy+9,time,kutta))
-i_1 = np.zeros((pregnancy+9,time,kutta))
-z_1 = np.zeros((pregnancy+9,time,kutta))
+# s_1 = np.zeros((pregnancy+9,time,kutta))
+# v_1 = np.zeros((pregnancy+9,time,kutta))
+# i_1 = np.zeros((pregnancy+9,time,kutta))
+# z_1 = np.zeros((pregnancy+9,time,kutta))
 
 ds = np.zeros((pregnancy,time - 1,kutta))
 dv = np.zeros((pregnancy,time - 1,kutta))
@@ -49,6 +49,23 @@ incremento_i = np.zeros((pregnancy,time))
 incremento_v = np.zeros((pregnancy,time))
 incremento_z = np.zeros((pregnancy,time))
 
+###############################
+
+s_1 = np.zeros((pregnancy,time,kutta))
+v_1 = np.zeros((pregnancy,time,kutta))
+i_1 = np.zeros((pregnancy,time,kutta))
+z_1 = np.zeros((pregnancy,time,kutta))
+
+ds_1 = np.zeros((pregnancy,time - 1,kutta))
+dv_1 = np.zeros((pregnancy,time - 1,kutta))
+di_1 = np.zeros((pregnancy,time - 1,kutta))
+dz_1 = np.zeros((pregnancy,time - 1,kutta))
+
+incremento_s_1 = np.zeros((pregnancy,time))
+incremento_i_1 = np.zeros((pregnancy,time))
+incremento_v_1 = np.zeros((pregnancy,time))
+incremento_z_1 = np.zeros((pregnancy,time))
+
 dados_2 = np.zeros((pregnancy+9,time,kutta))
 dados_3 = np.zeros((pregnancy+9,time,kutta))
 dados_4 = np.zeros((pregnancy+9,time,kutta))
@@ -59,71 +76,49 @@ i[:,0,0] = 10
 v[:,0,0] = 165
 z[:,0,0] = 2000 * wks_z
 
+s_1[:,0,0] = 250 * wks_s
+i_1[:,0,0] = 10
+v_1[:,0,0] = 165
+z_1[:,0,0] = 2000 * wks_z
+
 dados_x = np.zeros((pregnancy+55,time,kutta))
 
-def graph_generator(eixo_x,eixo_y,a_x,t,dados,nome):
+def graph_generator_3(eixo_x,eixo_y,a_x,t,dados,dados2,nome):
     fig, ax = plt.subplots()
+    # plt.title(nome)
     plt.subplots_adjust(right=0.97, top=0.97)
     plt.xlabel(eixo_x, fontsize = 12, labelpad = 4)
     plt.ylabel(eixo_y,  fontsize = 12, labelpad = 6)
-    plt.plot(t,dados[0,:,0],'#0000ff',label = 'Pre-pregnancy',linewidth = '1.8')
-    plt.plot(t,dados[1,:,0],'#00df00',label='8 weeks',linewidth = '1.8')
-    plt.plot(t,dados[2,:,0],'#ff00ff',label='16 weeks',linewidth = '1.8')
-    plt.plot(t,dados[3,:,0],'#ff4500',label='24 weeks',linewidth = '1.8')
+    plt.plot(t,dados[0,:,0],'#00c2b0',label = 'Com antiviral',linewidth = '1.8')
+    plt.plot(t,dados2[0,:,0],'#ff8201',label = 'Sem antiviral',linewidth = '1.8')
+    plt.axvline(x = 5, color = 'b', linestyle = '--', linewidth = 1.2,label = 'Administração do antiviral')
+    # plt.scatter(t[100],329, marker = 'X', color = 'b')                #pontos no gráfic
     leg = ax.legend()
     plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5)
     name = nome + '[' + str(a_x) + '].png'
     plt.savefig(name,format='png')
     return 0
-
-def graph_generator_2(eixo_x,eixo_y,t,dados,nome):
-    fig, ax = plt.subplots()
-    plt.subplots_adjust(right=0.97, top=0.97)
-    plt.xlabel(eixo_x, fontsize = 12, labelpad = 4)
-    plt.ylabel(eixo_y,  fontsize = 12, labelpad = 6)
-    plt.plot(t,dados[0,:,0],'#0000ff',label = 'Stage 1',linewidth = '1.8')
-    plt.plot(t,dados[1,:,0],'#30C636',label='Stage 2',linewidth = '1.8')
-    plt.plot(t,dados[2,:,0],'#cc0000',label='Stage 3',linewidth = '1.8')
-    leg = ax.legend()
-    plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5)
-    name = nome + '.png'
-    plt.savefig(name,format='png')
-    return 0
-
-def graph_generator_3(eixo_x,eixo_y,a_x,t,dados,nome):
-    fig, ax = plt.subplots()
-    plt.subplots_adjust(right=0.97, top=0.97)
-    plt.xlabel(eixo_x, fontsize = 12, labelpad = 4)
-    plt.ylabel(eixo_y,  fontsize = 12, labelpad = 6)
-    plt.plot(t,dados[0,:,0],'#0000ff',label = 'Pre-pregnancy',linewidth = '1.8')
-    leg = ax.legend()
-    plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5)
-    name = nome + '[' + str(a_x) + '].png'
-    plt.savefig(name,format='png')
-    return 0
-
-def stages_graphs(a_x,dados,flag):
-    if flag == 'i_m':
-        a_x+= 12
-    elif flag == 'z_p':
-        a_x+= 24
-    elif flag == 'l_p':
-        a_x+= 36
-    dados_x[a_x,:,0] = dados[0,:,0]
-    dados_x[a_x+3,:,0] = dados[1,:,0] 
-    dados_x[a_x+6,:,0] = dados[2,:,0] 
-    dados_x[a_x+9,:,0] = dados[3,:,0] 
-
-    return
 
 for w in range(1):
-    print(w)
     for n in range(time - 1):
+        # print(n) 
+        if(n <=500):#100/133/200/300/400/500
+            remedio = 1
+        else:
+            remedio = 0
+        # if (n>100 and n<200): 
+        #         print(v[0,n,0],n)
         for r in range(kutta):
+            
             ds[w,n,r] = wks_mu[w] - alpha * s[w,n,r] - a[2] * s[w,n,r] * v[w,n,r]
             di[w,n,r] = a[2] * s[w,n,r] * v[w,n,r] - beta * i[w,n,r] - nu * i[w,n,r] * z[w,n,r]
-            dv[w,n,r] = k * i[w,n,r] - gamma * v[w,n,r] - a[2] * s[w,n,r] * v[w,n,r]
+            dv[w,n,r] = remedio * k * i[w,n,r] - gamma * v[w,n,r] - a[2] * s[w,n,r] * v[w,n,r]
             dz[w,n,r] = wks_eta[w] + wks_c[w] * i[w,n,r] + wks_d[w] * i[w,n,r] * z[w,n,r] - delta * z[w,n,r]
+
+            ds_1[w,n,r] = wks_mu[w] - alpha * s_1[w,n,r] - a[2] * s_1[w,n,r] * v_1[w,n,r]
+            di_1[w,n,r] = a[2] * s_1[w,n,r] * v[w,n,r] - beta * i_1[w,n,r] - nu * i_1[w,n,r] * z_1[w,n,r]
+            dv_1[w,n,r] = k * i_1[w,n,r] - gamma * v[w,n,r] - a[2] * s_1[w,n,r] * v_1[w,n,r]
+            dz_1[w,n,r] = wks_eta[w] + wks_c[w] * i_1[w,n,r] + wks_d[w] * i_1[w,n,r] * z_1[w,n,r] - delta * z_1[w,n,r]
 
             if r < (kutta - 1):
                 s[w,n,r+1] = s[w,n,r] + h1 * ds[w,n,r]
@@ -131,17 +126,32 @@ for w in range(1):
                 v[w,n,r+1] = v[w,n,r] + h1 * dv[w,n,r]
                 z[w,n,r+1] = z[w,n,r] + h1 * dz[w,n,r]
 
+                s_1[w,n,r+1] = s_1[w,n,r] + h1 * ds_1[w,n,r]
+                i_1[w,n,r+1] = i_1[w,n,r] + h1 * di_1[w,n,r]
+                v_1[w,n,r+1] = v_1[w,n,r] + h1 * dv_1[w,n,r]
+                z_1[w,n,r+1] = z_1[w,n,r] + h1 * dz_1[w,n,r]
+
         incremento_s[w,n] = (h/6) * (ds[w,n,0] + 2 * ds[w,n,1] + 2 * ds[w,n,2] + ds[w,n,3])
         incremento_i[w,n] = (h/6) * (di[w,n,0] + 2 * di[w,n,1] + 2 * di[w,n,2] + di[w,n,3])
         incremento_v[w,n] = (h/6) * (dv[w,n,0] + 2 * dv[w,n,1] + 2 * dv[w,n,2] + dv[w,n,3])
         incremento_z[w,n] = (h/6) * (dz[w,n,0] + 2 * dz[w,n,1] + 2 * dz[w,n,2] + dz[w,n,3])
 
+        incremento_s_1[w,n] = (h/6) * (ds_1[w,n,0] + 2 * ds_1[w,n,1] + 2 * ds_1[w,n,2] + ds_1[w,n,3])
+        incremento_i_1[w,n] = (h/6) * (di_1[w,n,0] + 2 * di_1[w,n,1] + 2 * di_1[w,n,2] + di_1[w,n,3])
+        incremento_v_1[w,n] = (h/6) * (dv_1[w,n,0] + 2 * dv_1[w,n,1] + 2 * dv_1[w,n,2] + dv_1[w,n,3])
+        incremento_z_1[w,n] = (h/6) * (dz_1[w,n,0] + 2 * dz_1[w,n,1] + 2 * dz_1[w,n,2] + dz_1[w,n,3])
+
         s[w,n+1,0] = s[w,n,0] + incremento_s[w,n]
         i[w,n+1,0] = i[w,n,0] + incremento_i[w,n]
         v[w,n+1,0] = v[w,n,0] + incremento_v[w,n]
         z[w,n+1,0] = z[w,n,0] + incremento_z[w,n]
+        
+        s_1[w,n+1,0] = s_1[w,n,0] + incremento_s_1[w,n]
+        i_1[w,n+1,0] = i_1[w,n,0] + incremento_i_1[w,n]
+        v_1[w,n+1,0] = v_1[w,n,0] + incremento_v_1[w,n]
+        z_1[w,n+1,0] = z_1[w,n,0] + incremento_z_1[w,n]
 
-graph_generator_3('Time (days)',u'Monocytes/\u03bcL',0,t,s,'Susceptible_monocyte_population')
-graph_generator_3('Time (days)',u'Monocytes/\u03bcL',0,t,i,'Population_of_infected_monocytes')
-graph_generator_3('Time (days)',u'Viral particles/\u03bcL',0,t,v,'Zika_virus_population')
-graph_generator_3('Time (days)',u'T-Lymphocytes/\u03bcL',0,t,z,'T-lymphocyte_population')
+graph_generator_3('Tempo (dias)',u'Monócitos/\u03bcL',0,t,s,s_1,'Susceptible_monocyte_population')
+graph_generator_3('Tempo (dias)',u'Monócitos/\u03bcL',0,t,i,i_1,'Population_of_infected_monocytes')
+graph_generator_3('Tempo (dias)',u'Partículas virais/\u03bcL',0,t,v,v_1,'Zika_virus_population')
+graph_generator_3('Tempo (dias)',u'Linfócitos T/\u03bcL',0,t,z,z_1,'T-lymphocyte_population')
